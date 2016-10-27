e45123432c69b182a01d251b34fcf83c829a37ec
ljshj <jinshanl@gmail.com>
2016-10-27 08:22:08 +0800

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Show a loading spinner while application code is downloading.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/dropfen/meteor-app-loader',
+  git: 'https://github.com/ezing/meteor-app-loader',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
